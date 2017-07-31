511e82ac3b98978d569a017aa4e37394ab9e2111
jake frey <jakefreydesign@gmail.com>
2017-07-31 12:01:43 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-
+ $(document).on('scroll', function() {
+   if ($(document).scrollTop() > 500) {
+     $('.mark').addClass('stuck');
+   }
+   else {
+     $('.mark').removeClass('stuck');
+   }
+ });
